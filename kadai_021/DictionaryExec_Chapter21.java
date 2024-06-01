fe502b7c4bc809dd5d694582d9d287c76db559d0
Gen1001 <genshiro13@gmail.com>
2024-06-01 18:59:49 +0900

    kadai_021を修正

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,27 +1,11 @@
 package kadai_021;
 
-import java.util.HashMap;
-
 public class DictionaryExec_Chapter21 {
-	public void DictionaryExec(String[] array) {
-		HashMap<String,String> englishDictionary = new HashMap<String,String>();
-		englishDictionary.put("apple", "りんご");
-		englishDictionary.put("peach", "桃");
-		englishDictionary.put("banana", "バナナ");
-		englishDictionary.put("lemon", "レモン");
-		englishDictionary.put("pear", "梨");
-		englishDictionary.put("kiwi", "キウィ");
-		englishDictionary.put("strawberry", "いちご");
-		englishDictionary.put("grape", "ぶどう");
-		englishDictionary.put("muscat", "マスカット");
-		englishDictionary.put("cherry", "さくらんぼ");
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
 		
-		for (String name: array) {
-			if (name == "apple" || name == "peach" || name == "banana" || name == "lemon" || name == "pear" || name == "kiwi" || name == "straberry" || name == "grape" || name == "muscat" || name == "cherry") {
-				System.out.println(name + "の意味は" + englishDictionary.get(name));
-			} else {
-				System.out.println(name + "は辞書に存在しません");
-			}
-		}
+		String[] array = {"apple", "banana", "grape", "orange"};
+		
+		dictionary.DictionaryExec(array);
 	}
 }
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -1,11 +1,27 @@
 package kadai_021;
 
+import java.util.HashMap;
+
 public class Dictionary_Chapter21 {
-	public static void main(String[] args) {
-		DictionaryExec_Chapter21 dictionary = new DictionaryExec_Chapter21();
+	public void DictionaryExec(String[] array) {
+		HashMap<String,String> englishDictionary = new HashMap<String,String>();
+		englishDictionary.put("apple", "りんご");
+		englishDictionary.put("peach", "桃");
+		englishDictionary.put("banana", "バナナ");
+		englishDictionary.put("lemon", "レモン");
+		englishDictionary.put("pear", "梨");
+		englishDictionary.put("kiwi", "キウィ");
+		englishDictionary.put("strawberry", "いちご");
+		englishDictionary.put("grape", "ぶどう");
+		englishDictionary.put("muscat", "マスカット");
+		englishDictionary.put("cherry", "さくらんぼ");
 		
-		String[] array = {"apple", "banana", "grape", "orange"};
-		
-		dictionary.DictionaryExec(array);
+		for (String name: array) {
+			if (englishDictionary.containsKey(name)) {
+				System.out.println(name + "の意味は" + englishDictionary.get(name));
+			} else {
+				System.out.println(name + "は辞書に存在しません");
+			}
+		}
 	}
 }
